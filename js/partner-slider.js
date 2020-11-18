7469dc163afeef057bdc649e35d830d2cabf0b69
reine-work <62882259+reine-work@users.noreply.github.com>
2020-11-18 16:45:30 +0800

    Updated copy

--- js/partner-slider.js
+++ js/partner-slider.js
@@ -1,3 +1,76 @@
+jQuery(document).ready(function() {
+    jQuery('.slider-home-payments').owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        smartSpeed: 1500,
+        autoWidth: true,
+        slideTransition: 'linear',
+        responsive: {
+            0: {
+                items: 2
+            },
+            600: {
+                items: 4
+            },
+            1200: {
+                items: 5
+            },
+            1366: {
+                items: 5
+            },
+            1400: {
+                items: 5
+            },
+            1441: {
+                items: 9
+            }
+        }
+    });
+
+
+    jQuery('.slider-home-payments').trigger('play.owl.autoplay', [1500]);
+
+    function setSpeed() {
+        jQuery('.slider-home-payments').trigger('play.owl.autoplay', [1500]);
+    }
+
+    setTimeout(setSpeed, 10);
+});
+
+// $('.slider-home-payments').owlCarousel({
+//     loop: true,
+//     margin: 10,
+//     nav: false,
+//     dots: false,
+//     autoplay: true,
+//     mouseDrag: false,
+//     autoplayTimeout: 2000,
+//     smartSpeed: 1000,
+//     autoplayHoverPause: true,
+//     responsive: {
+//         0: {
+//             items: 2
+//         },
+//         600: {
+//             items: 4
+//         },
+//         1200: {
+//             items: 5
+//         },
+//         1366: {
+//             items: 5
+//         },
+//         1400: {
+//             items: 5
+//         },
+//         1441: {
+//             items: 8
+//         }
+//     }
+// });
+
+
 $('.slider-home-partners').owlCarousel({
     loop: true,
     margin: 10,
@@ -65,7 +138,8 @@ $('.slider-home-feedbacks').owlCarousel({
     nav: false,
     dots: false,
     autoplay: true,
-    autoplayTimeout: 1500,
+    smartSpeed: 2000,
+    autoplayTimeout: 4000,
     autoplayHoverPause: true,
     responsive: {
         0: {
