62e6b54c7dd7b1dd3192b5fa797ef5f9a6a0a60c
reine-work <62882259+reine-work@users.noreply.github.com>
2021-04-05 11:22:06 +0800

    Updated copy

--- js/selfie-camera.js
+++ js/selfie-camera.js
@@ -1,42 +1,10 @@
-// var video = document.getElementById('selfie-camera');
-
-// if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-//     navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
-//         video.srcObject = stream;
-//         video.play();
-//     });
-// }
-
-// // capture image
-// var canvas = document.getElementById('captured-selfie');
-// var context = canvas.getContext('2d');
-// var video = document.getElementById('selfie-camera');
-
-// // trigger
-// document.getElementById("capture-btn").addEventListener("click", function() {
-//     context.drawImage(video, 0, 0, 640, 480);
-// });
-
-// video.addEventListener('loadedmetadata', function() {
-//     context.translate(video.videoWidth, 0);
-//     context.scale(-1, 1);
-// });
-
 (function() {
-    // The width and height of the captured photo. We will set the
-    // width to the value defined here, but the height will be
-    // calculated based on the aspect ratio of the input stream.
 
-    var width = 480; // We will scale the photo width to this
-    var height = 0; // This will be computed based on the input stream
-
-    // |streaming| indicates whether or not we're currently streaming
-    // video from the camera. Obviously, we start at false.
+    // Camera initial size
+    var width = 480;
+    var height = 0;
 
     var streaming = false;
-
-    // The various HTML elements we need to configure or control. These
-    // will be set by the startup() function.
 
     var video = null;
     var canvas = null;
@@ -44,12 +12,15 @@
     var startbutton = null;
 
     function startup() {
-        video = document.getElementById('video');
-        canvas = document.getElementById('canvas');
+        video = document.getElementById('selfie-stream');
+        canvas = document.getElementById('selfie-canvas');
         photo = document.getElementById('selfie-rendered');
-        startbutton = document.getElementById('startbutton');
+        startbutton = document.getElementById('capture-selfie-btn');
 
-        navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+        navigator.mediaDevices.getUserMedia({
+                video: true,
+                audio: false
+            })
             .then(function(stream) {
                 video.srcObject = stream;
                 video.play();
@@ -62,9 +33,6 @@
         video.addEventListener('canplay', function(ev) {
             if (!streaming) {
                 height = video.videoHeight / (video.videoWidth / width);
-
-                // Firefox currently has a bug where the height can't be read from
-                // the video, so we will make assumptions if this happens.
 
                 if (isNaN(height)) {
                     height = width / (4 / 3);
@@ -86,9 +54,6 @@
         clearphoto();
     }
 
-    // Fill the photo with an indication that none has been
-    // captured.
-
     function clearphoto() {
         var context = canvas.getContext('2d');
         context.fillStyle = "transparent";
@@ -98,11 +63,7 @@
         photo.setAttribute('src', data);
     }
 
-    // Capture a photo by fetching the current contents of the video
-    // and drawing it into a canvas, then converting that to a PNG
-    // format data URL. By drawing it on an offscreen canvas and then
-    // drawing that to the screen, we can change its size and/or apply
-    // other changes before drawing it.
+    // Capture selfie
 
     function takepicture() {
         var context = canvas.getContext('2d');
@@ -120,7 +81,5 @@
         }
     }
 
-    // Set up our event listener to run the startup process
-    // once loading is complete.
     window.addEventListener('load', startup, false);
 })();
